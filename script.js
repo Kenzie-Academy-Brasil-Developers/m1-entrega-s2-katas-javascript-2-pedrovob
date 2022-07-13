a721709fd55a7010ae3516535c1924cf588bad74
pedro victor <pvobentes@gmail.com>
2022-07-12 20:07:41 -0400

    correção s5-08

--- script.js
+++ script.js
@@ -1,30 +1,46 @@
-function add(x, y) {
-    return x + y
-}
-function multiply(x, y) {
-    return x * y
-}
-function power(x, y) {
-    return x ** y
-}
-function factorial(x) {
-    if (x < 0) 
-        return -1;
-  else if (x == 0) 
-      return 1;
-  else {
-      return (x * factorial(x - 1));
-  } 
+function add(num1, num2) {
+ let soma = num1 + num2
+
+return soma
 }
 
-for (let i = 1; i < 5; i++) {
-    for (let j = 1; j < 5; j++) {
 
-        console.log(`a soma de ${i} + ${j} = `,add(i,j))
-        console.log(`a multiplicação de ${i} x ${j} = `,multiply(i,j))
-        console.log(`a potencia de ${i} elevado a ${j} = `,power(i,j))
-        console.log(`o fatorial de ${i} = `,factorial(i))
-    }
+console.assert(add(3, 5) === 8, 'A função add não está funcionando como esperado');
+
+
+
+function multiply(num1, num2) {
+ let result = 0
+for (let counter = 1; counter <= num1; counter++) {
+ result += add(num2, 0)
+ }
+ return result
+}
+
+
+console.assert(multiply(4, 6) === 24, 'A função multiply não está funcionando como esperado');
+
+
+
+function power(x, n) {
+let result = x
+for (let counter = 1; counter < n; counter++) {
+
+ result = multiply(x, result)
+}
+  return result
+}
+
+console.assert(power(3, 4) === 81, 'A função power não está funcionando como esperado');
+
+
+function factorial(num) {
+if (num <= 1) {
+return 1
+} else {
+ return num * factorial(num - 1)
+ }
 
 }
 
+console.assert(factorial(5) === 120, 'A função factorial não está funcionando como esperado')
